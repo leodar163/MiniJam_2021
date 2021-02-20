0d80a6c3afd3e3ef254ccd1c2e3af724c41d1d7f
Yann Roirand <y.roirand@gmail.com>
2021-02-20 14:14:05 +0100

    relations entre les jauges - WIP
    dans fichier script InfoRessources

--- MiniJam2021/Assets/Script/InfoRessources.cs
+++ MiniJam2021/Assets/Script/InfoRessources.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 
 public class InfoRessources : MonoBehaviour
 {
@@ -16,18 +16,51 @@ public class InfoRessources : MonoBehaviour
         }
     }
 
+    [Header("Ressources")]
+    [Tooltip("barils de déchets nucléaires")]
+    [Range(0,6)]
     public float dechet = 0;
+    [Space(5)]
+    [Tooltip("barils de combustibles nucléaires")]
+    [Range(0,6)]
     public float uranium = 0;
+    [Space(5)]
+    [Tooltip("température au sein du réacteur")]
+    [Range(0,100)]
     public float temperature = 0;
+    [Space(5)]
+    [Tooltip("production en électricité")]
+    [Range(0,100)]
     public float elecProd = 0;
+    [Space(5)]
+    [Tooltip("production opti en électricité à atteindre")]
+    [Range(0,100)]
     public float elecExpect = 0;
+    [Space(5)]
+    [Tooltip("flotte pour refroidir le réacteur disponible")]
+    [Range(0,100)]
     public float flotte = 0;
+
+    [Header("Ratio")]
+    public float flotteRatio = 0.5f;
+    public float maxRessource = 100f;
+    public float minRessource = 0f;
+    public float maxBaril = 6f;
+    public float uraniumRatio = 0.1f;
+
+    [Header("Events")]
+    public UnityEvent barilDepleted = new UnityEvent();
 
     // Start is called before the first frame update
     void Start()
     {
         Chrono.Instance.chronoUpdate.AddListener(() => { 
             UpdateRessources(); 
+        });
+        barilDepleted.AddListener(() =>
+        {
+            if (dechet < maxBaril) dechet += 1;
+            else print("GAME OVER - trop de déchets");
         });
     }
 
@@ -39,6 +72,12 @@ public class InfoRessources : MonoBehaviour
 
     private void UpdateRessources()
     {
-
+        flotte = flotte + flotteRatio >= maxRessource ? maxRessource : flotte + flotteRatio;
+        float previousUranium = uranium;
+        uranium = uranium - uraniumRatio * elecProd > minRessource ? uranium - uraniumRatio * elecProd : minRessource;
+        if (Mathf.Floor(uranium)< Mathf.Floor(previousUranium))
+        {
+            barilDepleted.Invoke();
+        }
     }
 }
