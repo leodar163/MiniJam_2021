b42ae0288ec388c9e82408dabf2cf6a3b09ef0de
leoBG <leobg@users.noreply.github.com>
2021-02-20 20:05:51 +0100

    Score et Calender

--- MiniJam2021/Assets/Script/Score.cs
+++ MiniJam2021/Assets/Script/Score.cs
@@ -1,18 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class Score : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI scoreAdding;
+    [SerializeField] private TextMeshProUGUI totalScore;
+
+    public int score = 0;
+    [SerializeField] private float refreshRate = 0;
+    [Header("Threshold")]
+    [SerializeField] private float deltaBad = -30;
+    [SerializeField] private float deltaOk = -15;
+    [SerializeField] private float deltaGood = -5;
+    [SerializeField] private float deltaAngry = 10;
+    [Header("Rewards")]
+    [SerializeField] private int rewardBad = 0;
+    [SerializeField] private int rewardOk = 50;
+    [SerializeField] private int rewardGood = 100;
+    [SerializeField] private int rewardAngry = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Scoring());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private IEnumerator Scoring()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(refreshRate - 0.1f);
+
+            scoreAdding.text = "";
+
+            yield return new WaitForSeconds(0.1f);
+            int reward = DetermineReward();
+
+            score += reward;
+
+            scoreAdding.text = reward > 0 ? "+" + reward : "-" + reward;
+            totalScore.text = "" + score;
+        }
+    }
+
+    private int DetermineReward()
+    {
+        float delta = InfoRessources.Instance.elecProd - InfoRessources.Instance.elecExpect;
+
+        if (delta >= deltaAngry) return rewardAngry;
+        else if (delta >= deltaGood) return rewardGood;
+        else if (delta >= deltaOk) return rewardOk;
+
+        return rewardBad;
     }
 }
---
+++ MiniJam2021/Assets/Script/Time/Calendar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Calendar : MonoBehaviour
+{
+    private int hour = 0;
+    private int minute = 0;
+    private int day = 0;
+
+    [SerializeField] private TextMeshProUGUI txtDay;
+    [SerializeField] private TextMeshProUGUI txtHour;
+    [SerializeField] private TextMeshProUGUI txtMinute;
+
+
+    private void Start()
+    {
+        StartCoroutine(TikTak());
+    }
+
+    private IEnumerator TikTak()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1);
+            minute += 30;
+
+            MakeCalendar();
+        }
+    }
+
+    private void MakeCalendar()
+    {
+        txtMinute.text = minute % 60 == 0 ? "00" : "" +  (minute % 60);
+        txtHour.text = "" + (minute / 60 % 24);
+        txtDay.text = "" + (minute / 60 / 24);
+    }
+}
