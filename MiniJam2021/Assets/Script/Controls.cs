001d661ebf593a7afc0252cc5ae843388c802029
leoBG <leobg@users.noreply.github.com>
2021-02-20 15:02:56 +0100

    Débuts contrôles

---
+++ MiniJam2021/Assets/Script/Controls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    [SerializeField] private KeyCode pauseCafe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Chrono.Instance.chronoUpdate.AddListener(() =>
+        {
+            CheckControls();
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void CheckControls()
+    {
+        if(Input.GetKey(pauseCafe))
+        {
+
+        }
+    }
+}
